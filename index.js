54b2e18aa95d60154238b034c0024c1f674abc93
xhWang00 <wangxiaohang2000@outlook.com>
2022-12-26 16:31:29 -0800

    Adding index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hello world");
+});
+
+app.listen(8000, () => {
+    console.log("Server is running at port 8000.");
+});
